219adf87c5618a1310af745e657d4c252dd74e7f
Roy <goutomroy770@gmail.com>
2023-01-06 23:51:25 +0600

    homepage

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -13,7 +13,48 @@ class _HOMEState extends State<HOME> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: SafeArea(
+          child: Stack(
+        children: [
+          CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                leading: Icon(Icons.abc),
+              ),
+              SliverPersistentHeader(
+                delegate: SliverDelegate(
+                    child: TextField(
+                  decoration: InputDecoration(hintText: 'JSDJDJD'),
+                )),
+              )
+            ],
+          )
+        ],
+      )),
     );
+  }
+}
+
+class SliverDelegate extends SliverPersistentHeaderDelegate {
+  Widget child;
+  SliverDelegate({required this.child});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return child;
+  }
+
+  @override
+  double get maxExtent => 70;
+
+  @override
+  double get minExtent => 70;
+
+  @override
+  bool shouldRebuild(SliverDelegate oldDelegate) {
+    return oldDelegate.maxExtent != 70 ||
+        oldDelegate.minExtent != 70 ||
+        child != oldDelegate.child;
   }
 }
